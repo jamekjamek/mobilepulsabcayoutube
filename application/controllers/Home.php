58c28451938cab7ce901a5fade9aff4d82bbcbcd
Jamekjamek <wargaindonesia125@gmail.com>
2021-02-17 12:17:05 +0700

    Pertemuan III Penginstallan Template HOUND

---
+++ application/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        // $this->load->view('template/header');
+        // $this->load->view('template/topbar');
+        // $this->load->view('template/sidebar');
+        // $this->load->view('home/index');
+        // $this->load->view('template/footer');
+        $page  =    '/home/index';
+        page($page);
+    }
+}
+
+/* End of file Home.php */
