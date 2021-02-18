710064d3f521a2c4a00f6ff5e28175895d148f3b
Jamekjamek <wargaindonesia125@gmail.com>
2021-02-18 21:00:05 +0700

    Sudah selesai record malah error

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,18 +1,146 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+use GuzzleHttp\Exception\BadResponseException;
+
 class Home extends CI_Controller
 {
 
+    private static $main_url        = 'https://sandbox.bca.co.id';
+    private static $client_id       = '3ec0e446-1ab6-4b18-b823-9cb174b8f471';
+    private static $client_secret   = '8e4b885a-6bd6-48aa-a290-e28ca3460a67';
+    private static $api_key         = '99319500-625f-439e-af9d-ca6d4ca9a37c';
+    private static $api_secret      = 'ed936a07-08cd-4758-a0d2-971756ecb504';
+    private static $access_token    = null;
+    private static $timestamp       = null;
+    private static $corporate_id    = 'BCAAPI2016';
+    private static $account_number  = '0201245680';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->guzzle       = new \GuzzleHttp\Client;
+        self::$timestamp    = date('o-m-d') . 'T' . date('H:i:s') . '.' . substr(date('u'), 0, 3) . date('P');
+        $this->getToken();
+    }
+
+
     public function index()
     {
-        // $this->load->view('template/header');
-        // $this->load->view('template/topbar');
-        // $this->load->view('template/sidebar');
-        // $this->load->view('home/index');
-        // $this->load->view('template/footer');
-        $page  =    '/home/index';
-        page($page);
+
+        if ($this->input->get('startDate') && $this->input->get('endDate')) {
+            $startDate  = $this->input->get('startDate');
+            $endDate    = $this->input->get('endDate');
+        } else {
+            // $startDate  = date('Y-m-') . '01';
+            // $endDate  = date('Y-m-d');
+            $startDate  = '2016-08-29';
+            $endDate    = '2016-09-01';
+        }
+
+
+        $results        = $this->getStatements($startDate, $endDate);
+        if (@$results->ErrorCode) {
+            $alert  = ' <div class="alert alert-danger alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button> ' . $results->ErrorMessage->Indonesian . '
+                        </div>';
+
+            $this->session->set_flashdata('error', $alert);
+            redirect('bca');
+        } else {
+            $statements = $results->Data;
+            $danamasuk  = 0;
+            foreach ($statements as $mutasi) {
+                if ($mutasi->TransactionType == 'C') {
+                    $danamasuk += $mutasi->TransactionAmount;
+                }
+            }
+        }
+
+        $data   = [
+            'title'         => 'HOME BCA',
+            'saldo'         => $this->getBalance(),
+            'danamasuk'     => $danamasuk,
+            'statements'    => $statements
+        ];
+        $page   =    '/home/index';
+        page($page, $data);
+    }
+
+    private function getToken()
+    {
+        $url        = '/api/oauth/token';
+        $output     = $this->guzzle->request('POST', self::$main_url . $url, [
+            'verify'        => false,
+            'headers'       => [
+                'Authorization'     => 'Basic ' . base64_encode(self::$client_id . ':' . self::$client_secret),
+                'Content-Type'      => 'application/x-www-form-urlencoded'
+            ],
+            'form_params'   => [
+                'grant_type'        => 'client_credentials'
+            ]
+        ]);
+        $output     = json_decode($output->getBody()->getContents(), true);
+        return self::$access_token = $output['access_token'];
+    }
+
+
+    private function getSignature($method, $relative_url, $request_body = '')
+    {
+        $request_body   = strtolower(hash('sha256', $request_body));
+        $stringToSign   = $method . ':' . $relative_url . ':' . self::$access_token . ':' . $request_body . ':' . self::$timestamp;
+        $signature      = hash_hmac('sha256', $stringToSign, self::$api_secret);
+        return $signature;
+    }
+
+
+    private function getBalance()
+    {
+        $url        =   '/banking/v3/corporates/' . self::$corporate_id . '/accounts/' . self::$account_number;
+        $method     = 'GET';
+
+        $output     = $this->guzzle->request($method, self::$main_url . $url, [
+            'verify'    => false,
+            'headers'   => [
+                'X-BCA-Signature'   => $this->getSignature($method, $url),
+                'CorporateID'       => self::$corporate_id,
+                'Authorization'     => 'Bearer ' . self::$access_token,
+                'X-BCA-Key'         => self::$api_key,
+                'AccountNumber'     => self::$account_number,
+                'X-BCA-Timestamp'   => self::$timestamp
+            ]
+        ]);
+
+        $output = json_decode($output->getBody()->getContents(), true);
+        return $output['AccountDetailDataSuccess'][0]['Balance'];
+    }
+
+
+    private function getStatements($startDate, $endDate)
+    {
+
+        $url        =   '/banking/v3/corporates/' . self::$corporate_id . '/accounts/' . self::$account_number . '/statements?EndDate=' . $endDate . '&StartDate=' . $startDate;
+        $method     = 'GET';
+        try {
+            $output     = $this->guzzle->request($method, self::$main_url . $url, [
+                'verify'    => false,
+                'headers'   => [
+                    'X-BCA-Signature'   => $this->getSignature($method, $url),
+                    'StartDate'         => $startDate,
+                    'EndDate'           => $endDate,
+                    'CorporateID'       => self::$corporate_id,
+                    'Authorization'     => 'Bearer ' . self::$access_token,
+                    'X-BCA-Key'         => self::$api_key,
+                    'AccountNumber'     => self::$account_number,
+                    'X-BCA-Timestamp'   => self::$timestamp
+                ]
+            ]);
+            $output = json_decode($output->getBody()->getContents());
+        } catch (BadResponseException $exception) {
+            $output = json_decode($exception->getResponse()->getBody()->getContents());
+        }
+        return $output;
     }
 }
 
--- application/views/page/home/index.php
+++ application/views/page/home/index.php
@@ -5,19 +5,179 @@
         <!-- Title -->
         <div class="row heading-bg">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h5 class="txt-dark">blank page</h5>
+                <h5 class="txt-dark"><?= $title; ?></h5>
             </div>
             <!-- Breadcrumb -->
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="index.html">Dashboard</a></li>
-                    <li><a href="#"><span>speciality pages</span></a></li>
-                    <li class="active"><span>blank page</span></li>
+                    <li class="active"><span><?= $title; ?>e</span></li>
                 </ol>
             </div>
             <!-- /Breadcrumb -->
         </div>
         <!-- /Title -->
+
+        <!-- INFORMASI SALSO -->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel panel-default card-view pa-0">
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body pa-0">
+                            <div class="sm-data-box bg-red">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
+                                            <span class="weight-500 uppercase-font txt-light block font-13">SALDO</span>
+                                            <span class="txt-light block counter">
+                                                <?= "Rp ." . number_format($saldo, 2, ',', '.'); ?>
+                                            </span>
+                                        </div>
+                                        <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
+                                            <i class="zmdi zmdi-male-female txt-light data-right-rep-icon"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel panel-default card-view pa-0">
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body pa-0">
+                            <div class="sm-data-box bg-blue">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
+                                            <span class="weight-500 uppercase-font txt-light block">SALDO MASUK BULAN INI</span>
+                                            <span class="txt-light block counter"><?= "Rp ." . number_format($danamasuk, 2, ',', '.'); ?></span>
+                                        </div>
+                                        <div class="col-xs-6 text-center  pl-0 pr-0 pt-25  data-wrap-right">
+                                            <div id="sparkline_4" style="width: 100px; overflow: hidden; margin: 0px auto;"><canvas style="display: inline-block; width: 115px; height: 50px; vertical-align: top;" width="115" height="50"></canvas></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /INFORMASI SALDO -->
+
+        <!-- CEK MUTASI -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default card-view">
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <h6 class="panel-title txt-dark">CEK MUTASI REKENING</h6>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <div class="form-wrap">
+                                <form class="form-inline" method="GET" action="">
+                                    <div class="form-group mr-15">
+                                        <label class="control-label mr-10" for="start_date">Tanggal Awal</label>
+                                        <input type="date" class="form-control" id="start_date" name="startDate" required>
+                                    </div>
+                                    <div class="form-group mr-15">
+                                        <label class="control-label mr-10" for="end_date">Tanggal Akhir</label>
+                                        <input type="date" class="form-control" id="end_date" name="endDate" required>
+                                    </div>
+                                    <button type="submit" class="btn btn-success btn-anim"><i class="icon-rocket"></i><span class="btn-text">CARI</span></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <?= $this->session->flashdata('error'); ?>
+                </div>
+            </div>
+
+            <div class="col-sm-12">
+
+
+            </div>
+        </div>
+        <!-- /CEK MUTASI -->
+
+
+
+        <!-- INFORMASI MUTASI -->
+        <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="panel panel-default card-view panel-refresh">
+                    <div class="refresh-container">
+                        <div class="la-anim-1"></div>
+                    </div>
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <h6 class="panel-title txt-dark">DATA MUTASI REKENING</h6>
+                        </div>
+                        <div class="pull-right">
+                            <a href="#" class="pull-left inline-block refresh mr-15">
+                                <i class="zmdi zmdi-replay"></i>
+                            </a>
+                            <a href="#" class="pull-left inline-block full-screen mr-15">
+                                <i class="zmdi zmdi-fullscreen"></i>
+                            </a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body row pa-0">
+                            <div class="table-wrap">
+                                <div class="table-responsive">
+                                    <table class="table table-hover mb-0">
+                                        <thead>
+                                            <tr>
+                                                <th>TANGGAL</th>
+                                                <th>BRANCH CODE</th>
+                                                <th>TIPE TRANSAKSI</th>
+                                                <th>TOTAL</th>
+                                                <th>NAMA TRANSAKSI</th>
+                                                <th>TRAILER</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php foreach ($statements as $mutasi) : ?>
+                                                <tr>
+                                                    <td><span class="txt-dark weight-500"><?= $mutasi->TransactionDate; ?></span></td>
+                                                    <td><?= $mutasi->BranchCode; ?></td>
+                                                    <td><span class="txt-dark weight-500"><?= $mutasi->TransactionType; ?></span></td>
+                                                    <td>
+                                                        <span class="txt-dark weight-500"><?= "Rp ." . number_format($mutasi->TransactionAmount, 2, ',', '.'); ?></span>
+                                                    </td>
+                                                    <td>
+                                                        <span class="txt-dark"><?= $mutasi->TransactionName; ?></span>
+                                                    </td>
+                                                    <td>
+                                                        <span class="txt-dark"><?= $mutasi->Trailer; ?></span>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /INFORMASI MUTASI -->
+
+
 
         <!-- Footer -->
         <footer class="footer container-fluid pl-30 pr-30">
